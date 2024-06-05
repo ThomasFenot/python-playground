4f4c0998e063c42eb184470a9cc32f718ba50fdd
Thomas <fenotthomas@gmail.com>
2024-06-05 10:40:52 +0200

    Fix : Fixed config file and db call

--- api-playground/mongo-connector/init_mongo.py
+++ api-playground/mongo-connector/init_mongo.py
@@ -1,9 +1,29 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://fenotthomas:password@python-sandbox.j1wve4f.mongodb.net/?retryWrites=true&w=majority&appName=python-sandbox"
-# Create a new client and connect to the server
+import configparser
+import os
+
+config_file = os.path.join(os.getcwd(), 'python-playground\\api-playground\\config\\config.ini')
+
+if not os.path.exists(config_file):
+    raise FileNotFoundError(f"Config file '{config_file}' not found.")
+
+config = configparser.ConfigParser()
+
+config.read(config_file)
+
+if 'MongoDB' not in config:
+    raise KeyError("Ney 'MongoDB' not found in config file.")
+
+username = config['MongoDB']['username']
+password = config['MongoDB']['password']
+db_adress = config['MongoDB']['database_adress']
+app_name = config['MongoDB']['app_name']
+
+uri = f"mongodb+srv://{username}:{password}@{db_adress}/?retryWrites=true&w=majority&appName={app_name}"
+
 client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
+
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
